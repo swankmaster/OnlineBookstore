2fdda2e8544d58cacf7a33293d3cc4aaabee0cb7
Conner Bissell <37808662+cbbissell@users.noreply.github.com>
2021-03-30 13:57:03 -0400

    working update

--- online_bookstore/store/urls.py
+++ online_bookstore/store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('myCart/', views.edit_profile, name='myCart'),
-    path('orderHistory/', views.edit_profile, name='orderHistory'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html'), name='login'),
+    path('', auth_views.LogoutView.as_view(template_name='store/home.html'), name='logout'),
 ]
